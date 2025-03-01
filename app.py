d413ba608c05d9a79c7927ce5c890b4a716349a0
rajasimha-stack <rajasimhagr@gmail.com>
2025-03-02 02:32:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Debug Route to Check Static Files
+@app.route('/test-static/<path:filename>')
+def test_static(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
